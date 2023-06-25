8014ef502b52dfbdb513efa356f74a29fec3a0d3
Harry Ruiz <harryyadier@gmail.com>
2023-06-25 19:49:51 -0400

    added audio change

--- index.js
+++ index.js
@@ -9,8 +9,12 @@ audio.onplay = function() {
 input.onchange = function() {
     if (input.value === "PesoPluma") {
         body.style.backgroundImage = "url('/resources/portadas-bzrp/pesopluma.jpg')";
+        audio.src = "resources/Bizarap ft Peso Pluma – Bzrp Music Sessions, Vol. 55.mp3";
+        audio.play(); 
     } else if (input.value === "Rauw") {
         body.style.backgroundImage = "url('/resources/portadas-bzrp/rauw2.jpg')";
+        audio.src = "resources/Bizarap ft Rauw Alejandro – Bzrp Music Sessions, Vol. 56.mp3"; 
+        audio.play();
     }
 }
 
