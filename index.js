2eabe6f9fb9cf6fc841c0411108c1635d774b711
Harry Ruiz <harryyadier@gmail.com>
2023-06-25 21:03:03 -0400

    fixed some things

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const title = document.getElementById("title");
 const video = document.getElementById("video");
 
 const musicSessions = ["resources/Bizarap ft Peso Pluma – Bzrp Music Sessions, Vol. 55.mp3", "resources/Bizarap ft Rauw Alejandro – Bzrp Music Sessions, Vol. 56.mp3"];
-const musicSessions2 = ["url('/resources/portadas-bzrp/pesopluma.jpg')", "url('/resources/portadas-bzrp/rauw2.jpg')"];
+const musicSessions2 = ["url('resources/portadas-bzrp/pesopluma.jpg')", "url('resources/portadas-bzrp/rauw2.jpg')"];
 const musicSessions3 = ["PesoPluma", "Rauw"];
 const musicSessions4 = ["PESO PLUMA || BZRP Music Sessions #55", "RAUW ALEJANDRO || BZRP Music Sessions #56"];
-const videoSessions = ["/resources/videos-bzrp/pesopluma.mp4", "/resources/videos-bzrp/rauw.mp4"];
+const videoSessions = ["resources/videos-bzrp/pesopluma.mp4", "resources/videos-bzrp/rauw.mp4"];
 
 audio.onplay = function() {
     console.log(input.value)
@@ -16,16 +16,16 @@ audio.onplay = function() {
 
 input.onchange = function() {
     if (input.value === "PesoPluma") {
-        body.style.backgroundImage = "url('/resources/portadas-bzrp/pesopluma.jpg')";
+        body.style.backgroundImage = "url('resources/portadas-bzrp/pesopluma.jpg')";
         audio.src = "resources/Bizarap ft Peso Pluma – Bzrp Music Sessions, Vol. 55.mp3";
         title.innerHTML = "PESO PLUMA || BZRP Music Sessions #55";
-        video.src = "/resources/videos-bzrp/pesopluma.mp4";
+        video.src = "resources/videos-bzrp/pesopluma.mp4";
         audio.play(); 
     } else if (input.value === "Rauw") {
-        body.style.backgroundImage = "url('/resources/portadas-bzrp/rauw2.jpg')";
+        body.style.backgroundImage = "url('resources/portadas-bzrp/rauw2.jpg')";
         audio.src = "resources/Bizarap ft Rauw Alejandro – Bzrp Music Sessions, Vol. 56.mp3"; 
         title.innerHTML = "RAUW ALEJANDRO || BZRP Music Sessions #56";
-        video.src = "/resources/videos-bzrp/rauw.mp4";
+        video.src = "resources/videos-bzrp/rauw.mp4";
         audio.play();
     }
 }
